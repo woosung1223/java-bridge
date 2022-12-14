1d668c65a8657cfeafe6f7e1b537509c887e4979
woosung1223 <dntjd991223@naver.com>
2022-12-14 21:30:47 +0900

    feat: 입력을 위한 메세지를 출력하는 기능 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,17 +3,24 @@ package bridge.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final String READING_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private final String READING_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String READING_GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 선택해주세요. (재시도: R, 종료: Q)";
+
     public int readBridgeSize() {
+        System.out.println(READING_BRIDGE_SIZE_MESSAGE);
         String line = Console.readLine();
         return Integer.parseInt(line);
     }
 
     public String readMoving() {
+        System.out.println(READING_MOVING_MESSAGE);
         String line = Console.readLine();
         return line;
     }
 
     public String readGameCommand() {
+        System.out.println(READING_GAME_COMMAND_MESSAGE);
         String line = Console.readLine();
         return line;
     }
